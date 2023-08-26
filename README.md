2e7e914195100a94209e582f7adb404b97b03419
Ted Tran <tedtran2019@gmail.com>
2023-08-26 15:03:37 -0700

    Completed the Hero and Navbar, completing the 3D computer component and just installed Three.js

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
 ```
 ```
-npm install @react-three/fiber @react-three/drei maath react-parallax-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom
+npm install @react-three/fiber @react-three/drei maath react-parallax-tilt react-vertical-timeline-component @emailjs/browser framer-motion react-router-dom three
 ```
 
 ## Run the website
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styles } from '../styles'
 import { navLinks } from '../constants'
-import { logo, menu, close, ttLogo, boringLogo, cryingLogo } from '../assets'
+import { menu, close, ttLogo } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
 
   return (
-    <nav className={`${styles.paddingX}} w-full flex items-center py-5 fixed top-0
-    z-20 bg-primary`}>
+    <nav
+      className={`${styles.paddingX}} w-full flex items-center py-5 fixed top-0
+    z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link 
+        <Link
           to="/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
-          }}>
-            <img src={ttLogo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className="text-white text-[18px] font-bold cursor-pointer">
-              <span>Ted Tran</span></p>
+          }}
+        >
+          <img src={ttLogo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            <span>Ted Tran</span>
+          </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
-            key={link.id}
-            className={`${
-              active === link.title ? "text-white" : "text-secondary"
-            }
-              hover:text-white text-[18px] font-medium cursor-pointer`}
-            // onClick={() => {setActive(link.title);}}
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }
+              hover:text-white hover:text-[18px] hover:font-medium hover:cursor-pointer`}
+              onClick={() => {
+                setActive(link.title);
+              }}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
+
+        <div className="flex sm:hidden flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu }
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${toggle ? "flex" : "hidden"} 
+            p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-x1`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }
+                    hover:text-white font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
